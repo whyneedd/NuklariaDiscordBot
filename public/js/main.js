f3455664177fb08be0aad6d3a280a47ab3b6935c
Jordane <jordan2offshel@hotmaile.com>
2022-11-05 10:43:53 -0400

    By Me

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,38 @@
-const header_js = document.querySelector(".js-header");
+var _this = this;
+var header_js = document.querySelector(".js-header");
+window.addEventListener("scroll", function () {
+  if (_this.scrollY > 0) {
+    header_js.classList.add("active");
+  } else {
+    header_js.classList.remove("active");
+  }
+});
+var h_menu = document.querySelector(".header__header-list");
+var btn_menu = document.querySelector(".header__header-humbger");
+btn_menu.addEventListener("click", function () {
+  btn_menu.classList.toggle("active");
+  h_menu.classList.toggle("active");
+});
 
-window.addEventListener("scroll", () => {
-    if (this.scrollY > 0) {
-        header_js.classList.add("active");
-    } else {
-        header_js.classList.remove("active");
+var cookiebloc = document.querySelector(".cookie");
+
+setInterval(function() {
+    let cookieaccpted = localStorage.getItem("cookieaccepted");
+    if (cookieaccpted !== "yes") {
+        cookiebloc.classList.add("active");
+    } 
+    else {
+        cookiebloc.classList.remove("active");
     }
-})
+}, 3000);
 
-const h_menu = document.querySelector(".header__header-list");
-const btn_menu = document.querySelector(".header__header-humbger");
+var cookieblocbtn = document.querySelector(".cookie__cookie-btn");
 
-btn_menu.addEventListener("click", () => {
-    btn_menu.classList.toggle("active");
-    h_menu.classList.toggle("active");
+cookieblocbtn.addEventListener("click", () => {
+  cookiebloc.classList.remove("active");
+  localStorage.setItem("cookieaccepted", "yes");
+});
+
+window.addEventListener("load", () => {
+    localStorage.clear()
 })
